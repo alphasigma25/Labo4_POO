17c04e595582eb0eb0aa6924a28db42edf8b1660
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-19 15:32:34 +0200

    modif du main pour l'affichage

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "Field.h"
+#include "Displayer/TerminalDisplay.h"
 
 using namespace std;
 
 int main() {
    Field f(50,50,20,10);
 
-   cout << "hello" << endl;
+   TerminalDisplay td(&f);
+
+   td.display();
    return 0;
 }
