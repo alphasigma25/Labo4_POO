987bd6ae8f8f2ceb340ec601b33beb648fbc4693
PaulReeve717 <paulreeve717@gmail.com>
2021-06-01 14:52:50 +0200

    Changes by AnneSo

--- Utils/Coordinate.h
+++ Utils/Coordinate.h
@@ -1,7 +1,3 @@
-//
-// Created by AnneSo on 26.05.2021.
-//
-
 #ifndef L4_COORDINATE_H
 #define L4_COORDINATE_H
 
@@ -9,22 +5,44 @@
  * Classe représentant une coordonnée 2D
  */
 class Coordinate {
-int x;
-int y;
+   // coordonnées x et y
+   int x;
+   int y;
 
 public:
+   /**
+    * Vecteur 2D de coordonnées
+    * @param x
+    * @param y
+    */
    Coordinate(int x, int y);
 
+   /**
+    * additionne deux coordonnées (adition vectorielle champs par champs)
+    * @param other autre coordonnée
+    * @return coordonnée résulat de l'addition
+    */
    Coordinate operator+(const Coordinate& other) const;
+   /**
+    * additionne un entier à chaque composante d'une coordonnée
+    * @param int entier
+    * @return coordonnée résulat de l'addition
+    */
    Coordinate operator+(int) const;
+
+   /**
+    * soustrait une coordonnée à la coordonnée courante (adition vectorielle champs par champs)
+    * @param other autre coordonnée
+    * @return coordonnée résulat de l'addition
+    */
    Coordinate operator-(const Coordinate& other) const;
 
+   /**
+    * getters et setters pour les champs x et y
+    */
    int getX() const;
-
    void setX(int x);
-
    int getY() const;
-
    void setY(int y);
 };
 
--- Utils/randomGenerator.h
+++ Utils/randomGenerator.h
@@ -5,17 +5,29 @@
 #ifndef L4_RANDOMGENERATOR_H
 #define L4_RANDOMGENERATOR_H
 
-
+/**
+ * Cette classe représente un générateur de nombres aléatoires
+ */
 class randomGenerator {
 
    static randomGenerator rg;
 
 public :
+   /**
+    * crée un générateur d'entiers aléatoires
+    */
    randomGenerator();
 
-
+   /**
+    * @param min
+    * @param max
+    * @return une valeur aléatoire comprise entre min et max
+    */
    int getRand(int min, int max);
 
+   /**
+    * @return instance du générateur
+    */
    static randomGenerator& getGenerator();
 
 };
